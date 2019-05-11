54ee933d1ba7f3ec10b9082373080ebc1ba43c16
conni <c.buschka@arcusx.com>
2019-05-11 10:49:09 +0200

    Disabled dashboard ttl patch.

--- auto-install-k8s.sh
+++ auto-install-k8s.sh
@@ -172,7 +172,7 @@ kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | gre
 
 function deployDashboard() {
 curl -o /tmp/kubernetes-dashboard.yaml https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
-perl -i -pe 's#          - --auto-generate-certificates#          - --auto-generate-certificates\n          - --token-ttl=600m#g' /tmp/kubernetes-dashboard.yaml
+#perl -i -pe 's#          - --auto-generate-certificates#          - --auto-generate-certificates\n          - --token-ttl=600m#g' /tmp/kubernetes-dashboard.yaml
 kubectl apply -f /tmp/kubernetes-dashboard.yaml
 
 echo "Exec 'kubectl proxy' and go to http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/"
