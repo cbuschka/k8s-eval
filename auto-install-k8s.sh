f54701b08807ae2e06651e49af8784e880fc5f44
conni <c.buschka@arcusx.com>
2019-05-11 09:25:33 +0200

    More nginx props.

--- auto-install-k8s.sh
+++ auto-install-k8s.sh
@@ -203,7 +203,7 @@ kubectl apply -f /tmp/hello-deployment.yml
 
 #kubectl create deployment hello-node --image=gcr.io/hello-minikube-zero-install/hello-node
 #https://kubernetes.io/docs/concepts/services-networking/ingress/
-
+# https://github.com/kubernetes/ingress-nginx/blob/master/docs/user-guide/nginx-configuration/annotations.md
 cat - > /tmp/hello-service.yaml <<EOB
 apiVersion: v1
 kind: Service
@@ -228,6 +228,19 @@ metadata:
   annotations:
     nginx.ingress.kubernetes.io/rewrite-target: /
     nginx.ingress.kubernetes.io/ssl-redirect: "false"
+    nginx.ingress.kubernetes.io/http2-push-preload: "true"
+    nginx.ingress.kubernetes.io/configuration-snippet: |
+      more_set_headers "Request-Id: $req_id";
+    nginx.ingress.kubernetes.io/enable-cors: "true"
+    nginx.ingress.kubernetes.io/limit-connections: 5
+    nginx.ingress.kubernetes.io/limit-rps: 20
+    nginx.ingress.kubernetes.io/limit-rpm: 120
+    nginx.ingress.kubernetes.io/from-to-www-redirect: "false"
+    nginx.ingress.kubernetes.io/connection-proxy-header: "keep-alive"
+    nginx.ingress.kubernetes.io/enable-access-log: "false"
+    nginx.ingress.kubernetes.io/enable-rewrite-log: "true"
+    nginx.ingress.kubernetes.io/backend-protocol: "HTTP"
+    nginx.ingress.kubernetes.io/satisfy: "any"
 spec:
   rules:
   - host: hello.ws0
