7decffd342fa8ef7fba4adf63ff159de5785d3ae
conni <c.buschka@arcusx.com>
2019-05-11 09:45:51 +0200

    Changed dashboard session ttl.

--- auto-install-k8s.sh
+++ auto-install-k8s.sh
@@ -171,7 +171,10 @@ kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | gre
 }
 
 function deployDashboard() {
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
+curl -o /tmp/kubernetes-dashboard.yaml https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
+perl -i -pe 's#          - --auto-generate-certificates#          - --auto-generate-certificates\n          - --token-ttl=600m#g' /tmp/kubernetes-dashboard.yaml
+kubectl apply -f /tmp/kubernetes-dashboard.yaml
+
 echo "Exec 'kubectl proxy' and go to http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/"
 }
 
@@ -184,7 +187,7 @@ metadata:
   labels:
     app: hello
 spec:
-  replicas: 1
+  replicas: 3
   selector:
     matchLabels:
       app: hello
