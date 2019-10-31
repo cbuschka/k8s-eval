59de692deb9c14f2c2148f155198262d2bcf2840
conni <c.buschka@arcusx.com>
2019-10-31 08:03:24 +0100

    Added logging to 900_reset

--- 900_reset.sh
+++ 900_reset.sh
@@ -7,9 +7,12 @@ source $(dirname $0)/lib.include.sh
 
 printBanner "Resetting..."
 ssh ${user}@${host} "
+echo 'echo Resetting k8s cluster...'
 kubeadm reset
+echo 'Cleaning firewall rules...'
 iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 rm -rf /etc/kubernetes/
+echo 'Done.'
 "
 
 exit $?
